65f820f370cfc730cb1505d9f207bbaf2b26e752
Bartosz Kruba <bartosz.kruba@gmail.com>
2019-03-18 18:10:00 +0100

    Added model for testing purposes

---
+++ sql_with_java/src/models/Car.java
@@ -0,0 +1,106 @@
+package models;
+
+
+import datasource.annotations.Column;
+import datasource.annotations.Table;
+
+import java.time.LocalDateTime;
+
+public class Car {
+
+   @Column("_id")
+   private Long id;
+
+   @Column("plate_number")
+   private String plateNumber;
+
+   @Column("model")
+   private String model;
+
+   @Column("color")
+   private String color;
+
+   @Column("owner_id")
+   private Long ownerId;
+
+   @Column("desciption")
+   private String description;
+
+   @Column("total_distance")
+   private Long total_distance;
+
+   @Column("production_year")
+   private LocalDateTime productionYear;
+
+   @Table("cars")
+   public Car() {
+   }
+
+   public Long getId() {
+      return id;
+   }
+
+   public String getPlateNumber() {
+      return plateNumber;
+   }
+
+   public String getModel() {
+      return model;
+   }
+
+   public String getColor() {
+      return color;
+   }
+
+   public Long getOwnerId() {
+      return ownerId;
+   }
+
+   public String getDescription() {
+      return description;
+   }
+
+   public LocalDateTime getProductionYear() {
+      return productionYear;
+   }
+
+   public Car setId(Long id) {
+      this.id = id;
+      return this;
+   }
+
+   public Car setPlateNumber(String plateNumber) {
+      this.plateNumber = plateNumber;
+      return this;
+   }
+
+   public Car setModel(String model) {
+      this.model = model;
+      return this;
+   }
+
+   public Car setColor(String color) {
+      this.color = color;
+      return this;
+   }
+
+   public Car setOwnerId(Long ownerId) {
+      this.ownerId = ownerId;
+      return this;
+   }
+
+   public Car setDescription(String description) {
+      this.description = description;
+      return this;
+   }
+
+   public Car setTotal_distance(Long total_distance) {
+      this.total_distance = total_distance;
+      return this;
+   }
+
+   public Car setProductionYear(LocalDateTime productionYear) {
+      this.productionYear = productionYear;
+      return this;
+   }
+}
